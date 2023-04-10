c860f6a232f80288c86847a8e297fae115f76747
ekaranasuf <ekaranasuf@asteasolutions.com>
2023-04-10 09:56:01 +0100

    Add Block Button

---
+++ app/src/components/Common/Button/Block/styles.ts
@@ -0,0 +1,51 @@
+import { scale } from '../../../../styles/base';
+import {
+  advanceYellow,
+  advanceVoid,
+  lightGray,
+  lightVoid,
+  pure,
+} from '../../../../styles/colors';
+import { StyleSheet } from 'react-native';
+
+export const primaryColor = advanceVoid;
+export const secondaryColor = lightVoid;
+
+export const iconSize = scale(16);
+
+export default StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    minHeight: scale(48),
+    padding: scale(16),
+    margin: scale(16),
+    backgroundColor: advanceYellow,
+    borderColor: advanceYellow,
+    borderWidth: scale(1),
+    borderRadius: scale(4),
+  },
+  disabled: {
+    borderWidth: scale(1),
+    borderColor: lightGray,
+    backgroundColor: pure,
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: scale(24),
+    color: primaryColor,
+    fontWeight: '500',
+  },
+  disabledText: {
+    color: secondaryColor,
+  },
+  icon: {
+    marginRight: scale(12),
+  },
+  spinner: {
+    height: scale(12),
+    marginLeft: scale(4),
+    alignSelf: 'center',
+  },
+});
