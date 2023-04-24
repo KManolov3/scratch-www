aa56c2da662bd5f631b24ff19184aed6b160f9e1
pdyulgerov <pdyulgerov@asteasolutions.com>
2023-04-24 14:33:46 +0300

    added helper methods

---
+++ e2e-tests/methods/helpers.ts
@@ -0,0 +1,107 @@
+import { expect } from 'chai';
+import { generate } from 'randomstring';
+
+const WAIT_FOR_TIMEOUT = 3000;
+
+export function generateRandomString(
+  length = 12,
+  charset = 'alphanumeric',
+  readable = true
+) {
+  const randomString = generate({
+    length: length,
+    charset: charset,
+    readable: readable,
+  });
+
+  return randomString;
+}
+
+export async function withImplicitWait(context, implicit: number) {
+  await driver.setTimeout({ implicit });
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await context();
+  } finally {
+    await driver.setTimeout({ implicit: WAIT_FOR_TIMEOUT });
+  }
+}
+
+export async function waitFor(
+  selector: string,
+  timeout = WAIT_FOR_TIMEOUT
+): Promise<WebdriverIO.Element> {
+  console.log(`Waiting for Element: ${selector}`);
+  const element = await $(selector);
+  await element.waitForDisplayed({ timeout });
+  console.log('Wait for element success');
+  return element;
+}
+
+export async function waitForInvisible(element: string) {
+  console.log(`Waiting for element ${element} to become invisible`);
+  expect(
+    await $(element).waitForDisplayed({
+      timeout: WAIT_FOR_TIMEOUT,
+      reverse: true,
+    })
+  );
+  console.log('Element is invisible');
+}
+
+export async function waitAndClick(
+  selector: string,
+  timeout = WAIT_FOR_TIMEOUT
+) {
+  const element = await waitFor(selector, timeout);
+  await element.click();
+  console.log(`Clicked on element ${selector}`);
+}
+
+export async function setValue(selector: string, value?: string | number) {
+  if (value !== undefined) {
+    const element = await $(selector);
+    await element.setValue(value);
+    console.log(`Value ${value} is set to ${selector}`);
+  }
+}
+
+export async function clearAndSetValue(
+  selector: string,
+  value?: string | number
+) {
+  if (value !== undefined) {
+    await clearValue(selector);
+    const element = await $(selector);
+    await element.setValue(value);
+    console.log(`Value ${value} is set to ${selector}`);
+  }
+}
+
+export async function clearValue(selector: string) {
+  console.log(`Clearing value of ${selector}`);
+  // Select all, delete
+  await (await $(selector)).keys(['Control', 'a', 'Backspace']);
+}
+
+export async function expectElementValue(selector: string, expected: string) {
+  await waitFor(selector);
+  const displayed = await $(selector).getValue();
+  expect(
+    expected === displayed,
+    `Displayed value ${displayed} does not match expected ${expected}`
+  );
+}
+
+export async function expectElementText(selector: string, expected: string) {
+  await browser.waitUntil(
+    async () => {
+      const displayed = await $(selector).getText();
+      return displayed === expected;
+    },
+    {
+      timeout: 3000,
+      timeoutMsg: `Displayed text does not match expected ${expected}`,
+    }
+  );
+}
