151f662076d2e02601de0e836f1d1ea3449bb99c
pdyulgerov <pdyulgerov@asteasolutions.com>
2023-05-09 09:35:04 +0300

    start servers script with function

--- e2e-tests/start-servers.sh
+++ e2e-tests/start-servers.sh
@@ -5,24 +5,23 @@ source .env
 ALL_DEVICES=($(emulator -list-avds))
 export DEVICE_NAME=${ALL_DEVICES[0]}
 
+  start_servers() {
+    $(
+      $1 -t 'react-native' 'cd ../app; npm start' && 
+      $1 -t 'mock-server' 'cd ../mock-server; npm start' &&
+      $1 -t 'appium-server' 'appium' &&
+      $1 -t 'virtual-device' 'emulator @'$DEVICE_NAME''
+    )
+  }
+
 if [[ $OS == "Linux" || $OS == "MacOS" ]]
 then
-  start_servers="$(
-    ttab -t 'react-native' 'cd ../app; npm start' && 
-    ttab -t 'mock-server' 'cd ../mock-server; npm start' &&
-    ttab -t 'appium-server' 'appium' &&
-    ttab -t 'virtual-device' 'emulator @'$DEVICE_NAME''
-  )"
+  start_servers ttab
 fi
 
 if [[ $OS == "Windows" ]]
 then
-  start_servers="$(
-    wttab -t 'react-native' 'cd ../app; npm start' && 
-    wttab -t 'mock-server' 'cd ../mock-server; npm start' &&
-    wttab -t 'appium-server' 'appium' &&
-    wttab -t 'virtual-device' 'emulator @'$DEVICE_NAME''
-  )"
+  start_servers wttab
 fi
 
 
