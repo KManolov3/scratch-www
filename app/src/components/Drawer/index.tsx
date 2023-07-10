0c63682c2dd34a15b5cfa7f612a4a0ac94ad3f0d
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2023-07-10 15:39:33 +0300

    always show print front tags in drawer

--- app/src/components/Drawer/index.tsx
+++ app/src/components/Drawer/index.tsx
@@ -1,4 +1,3 @@
-import { compact } from 'lodash-es';
 import { ReactElement, useCallback, useMemo } from 'react';
 import {
   Pressable,
@@ -71,21 +70,23 @@ export function Drawer() {
 
   const { configHamburgerMenuAppFunctions } = useFlags();
 
-  const sections = useMemo(
+  const sections = useMemo<DrawerSection[]>(
     () => [
       {
         title: 'Item In Slots',
-        data: compact([
-          selectedItem
-            ? {
-                label: 'Print Front Tags',
-                onPress: () =>
-                  navigate('PrintFrontTag', { itemDetails: selectedItem }),
-              }
-            : undefined,
+        data: [
+          {
+            label: 'Print Front Tags',
+            onPress: () =>
+              selectedItem
+                ? navigate('PrintFrontTag', { itemDetails: selectedItem })
+                : InStoreAppsNative.navigateTo(
+                    '.activities.ItemLookupActivity',
+                  ),
+          },
           // { label: 'Backstock Moves' },
           // { label: 'Manage Backstock slot' },
-        ]),
+        ],
       },
       {
         title: 'Functions',
