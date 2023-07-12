f1723fd618e5b530792c3e54b6404b85c9fd8f7c
Ivan Arabadzhiyski <ivan.arabadzhiyski@advance-auto.com>
2023-07-12 10:58:49 +0300

    add regex for the messages

--- app/src/lib/apollo.ts
+++ app/src/lib/apollo.ts
@@ -1,14 +1,13 @@
 import { ApolloError } from '@apollo/client';
 
+const lengthRegex = /(SKU| UPC) value should be of length between/;
+const digitsRegex = /(SKU| UPC) should only contain digits/;
+
 export function isApolloNoResultsError(error: unknown) {
   return !!(
     error instanceof ApolloError &&
     (error.graphQLErrors[0]?.extensions.errorType === 'NOT_FOUND' ||
-      error.graphQLErrors[0]?.message.endsWith(
-        'should only contain digits 0-9',
-      ) ||
-      error.graphQLErrors[0]?.message.endsWith(
-        'SKU value should be of length between 1 and 9',
-      ))
+      error.graphQLErrors[0]?.message.match(digitsRegex) ||
+      error.graphQLErrors[0]?.message.match(lengthRegex))
   );
 }
